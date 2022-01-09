f5bf8742579b7423c8ebd0ba41652758da2143f9
Arpit Agarwal <arpitagarwal0708@gmail.com>
2022-01-09 09:19:11 +0530

    Implement Map Based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/aapetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.aapetclinic.services.map;
+
+import guru.springframework.aapetclinic.model.Vet;
+import guru.springframework.aapetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long>
+        implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        // super will point to AbstractMapService class implementation
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
