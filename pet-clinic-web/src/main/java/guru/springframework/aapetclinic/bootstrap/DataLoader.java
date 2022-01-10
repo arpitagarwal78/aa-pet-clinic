1e877e0eee2d53360d0d3f9875ba144ce77d38ff
Arpit Agarwal <arpitagarwal0708@gmail.com>
2022-01-10 15:09:22 +0530

    Add PetTypes in with Bootstrap - DataLoader class (startup). Closes #33

--- pet-clinic-web/src/main/java/guru/springframework/aapetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/aapetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.aapetclinic.bootstrap;
 
 import guru.springframework.aapetclinic.model.Owner;
+import guru.springframework.aapetclinic.model.PetType;
 import guru.springframework.aapetclinic.model.Vet;
 import guru.springframework.aapetclinic.services.OwnerService;
+import guru.springframework.aapetclinic.services.PetTypeService;
 import guru.springframework.aapetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,17 +15,31 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
 
     // owner service map and vet service map will be autowired automatically
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+
+
+    public DataLoader(OwnerService ownerService,
+                      VetService vetService,
+                      PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     // when Spring boot context is build, Spring boot will call this method
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog); // done to get id
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
