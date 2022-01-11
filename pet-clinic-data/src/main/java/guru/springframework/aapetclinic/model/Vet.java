8792ecba3a29db70924cab72e6549c00ec6a808c
Arpit Agarwal <arpitagarwal0708@gmail.com>
2022-01-11 11:36:40 +0530

    Convert Vets to JPA Entities, Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/aapetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/aapetclinic/model/Vet.java
@@ -1,10 +1,17 @@
 package guru.springframework.aapetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", // create a single table
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id")) // this will tell jpa how to navigate between two tables
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
