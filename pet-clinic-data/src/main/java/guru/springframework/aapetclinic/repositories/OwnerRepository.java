1533e49b8aa3decca29b1838e8d0775d09a99597
Arpit Agarwal <arpitagarwal0708@gmail.com>
2022-01-11 12:05:53 +0530

    Create Spring Data JPA Service for Owners, Closes #41

--- pet-clinic-data/src/main/java/guru/springframework/aapetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/aapetclinic/repositories/OwnerRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     // we can even use JpaRepository for more methods that internally extends CrudRepository
     // for now we have general needs
+
+    Owner findByLastName(String lastName); // automatically setup method
 }
